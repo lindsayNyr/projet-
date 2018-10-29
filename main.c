94ce639b42b090f205e3d2b9b01b11644640fc42
lindsayNyr <43404297+lindsayNyr@users.noreply.github.com>
2018-10-29 19:40:12 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -12,75 +12,121 @@
 
 
 
-#include <SDL/SDL.h> 
+
+#define TAILLE_TILE 32  // hauteur et largeur des tiles.
+
+
+#define LARGEUR_MAP 15  // nombre a afficher en x et y
+#define HAUTEUR_MAP 13
+
+
+	
 
 
 
-#define LARGEUR_TILE 32  // hauteur et largeur des tiles. 
-#define HAUTEUR_TILE 32 
-
-#define NOMBRE_BLOCS_LARGEUR 15  // nombre a afficher en x et y 
-#define NOMBRE_BLOCS_HAUTEUR 13 
-
-char* table[] = { 
-"222222222222222", 
-"0000013000000000", 
-"000000000000000", 
-"000000000000000", 
-"100000000111110", 
-"000000000000000", 
-"000000000000000", 
-"000000000000000", 
-"003400022220022", 
-"005600000000000", 
-"005600000000000", 
-"005600000000000", 
-"777777777777777"}; 
 
 
-void Afficher(SDL_Surface* screen,SDL_Surface* tileset,char** table,int nombre_blocs_largeur,int nombre_blocs_hauteur) 
-{ 
-    int i,j; 
-    SDL_Rect Rect_dest; 
-    SDL_Rect Rect_source; 
-    Rect_source.w = LARGEUR_TILE; 
-    Rect_source.h = HAUTEUR_TILE; 
-    for(i=0;i<nombre_blocs_largeur;i++) 
-    { 
-        for(j=0;j<nombre_blocs_hauteur;j++) 
-        { 
-            Rect_dest.x = i*LARGEUR_TILE; 
-            Rect_dest.y = j*HAUTEUR_TILE; 
-            Rect_source.x = (table[j][i]-'0')*LARGEUR_TILE; 
-            Rect_source.y = 0; 
-            SDL_BlitSurface(tileset,&Rect_source,screen,&Rect_dest); 
-        } 
-    } 
-    SDL_Flip(screen); 
-} 
 
-int main(int argc,char** argv) 
-{ 
-    SDL_Surface* screen,*tileset; 
-    SDL_Event event; 
-    SDL_Init(SDL_INIT_VIDEO);        // prepare SDL 
-    screen = SDL_SetVideoMode(LARGEUR_TILE*NOMBRE_BLOCS_LARGEUR, HAUTEUR_TILE*NOMBRE_BLOCS_HAUTEUR, 32,SDL_HWSURFACE|SDL_DOUBLEBUF); 
-    tileset = SDL_LoadBMP("tiles.bmp"); 
-    if (!tileset) 
-    { 
-        printf("Echec de chargement tileset1.bmp\n"); 
-        SDL_Quit(); 
-        system("pause"); 
-        exit(-1); 
-    } 
-    Afficher(screen,tileset,table,NOMBRE_BLOCS_LARGEUR,NOMBRE_BLOCS_HAUTEUR); 
+void Afficher(SDL_Surface* screen,SDL_Surface* tileset,int table[LARGEUR_MAP][HAUTEUR_MAP]){
+   
+    int i,j;
+    SDL_Rect Rect_dest;
+    SDL_Rect Rect_source;
+    Rect_source.w = TAILLE_TILE;
+    Rect_source.h = TAILLE_TILE;
+   
+    for(i=0;i<LARGEUR_MAP;i++){
+        
+        for(j=0;j<HAUTEUR_MAP;j++){
+    
+            Rect_dest.x = i*TAILLE_TILE;
+            Rect_dest.y = j*TAILLE_TILE;
+           
+            if (table[i][j] < 10){
+	            Rect_source.x = table[i][j]*TAILLE_TILE;
+	            Rect_source.y = 0;
+         	}
+         	
+         	if (table[i][j] > 10 && table[i][j] < 20){
+		        Rect_source.x = (table[i][j]-10)*TAILLE_TILE;
+		        Rect_source.y = TAILLE_TILE;
+        	}
+	        
+	        if (table[i][j] > 20 && table[i][j] < 30){
+		        Rect_source.x = (table[i][j]-20)*TAILLE_TILE;
+		        Rect_source.y = TAILLE_TILE*2;
+	     	 }
+		     
+		     if (table[i][j] > 30 && table[i][j] < 40){
+			    Rect_source.x = (table[i][j]-30)*TAILLE_TILE;
+			    Rect_source.y = TAILLE_TILE*3;
+		    }
+            
 
-    do  // attend qu'on appuie sur une touche. 
-    { 
-        SDL_WaitEvent(&event); 
-    } while (event.type!=SDL_KEYDOWN); 
+            SDL_BlitSurface(tileset,&Rect_source,screen,&Rect_dest);
+        }
+    }
+    SDL_Flip(screen);
+}
+
+
+
+int main(int argc,char** argv)
+{
+	int map[LARGEUR_MAP][HAUTEUR_MAP];
+	int i,j;
+	FILE* fichier = NULL;
+  	SDL_Surface* screen,*tileset;
+    SDL_Event event;
+    SDL_Init(SDL_INIT_VIDEO);        // prepare SDL
+    screen = SDL_SetVideoMode(TAILLE_TILE*LARGEUR_MAP, TAILLE_TILE*HAUTEUR_MAP, 32,SDL_HWSURFACE|SDL_DOUBLEBUF);
+    tileset = SDL_LoadBMP("tiles.bmp");
+   
+    if (!tileset){
+       
+        printf("Echec de chargement tileset.bmp\n");
+        SDL_Quit();
+        system("pause");
+        exit(-1);
+    }
+
+  	fichier = fopen("level.txt", "r");
+
+ 	 	if (fichier != NULL){
+    
+	     	for(i=0; i<HAUTEUR_MAP; i++){
+	      		for(j=0; j<LARGEUR_MAP; j++){
+
+	 				 fscanf(fichier,"%d", &map[j][i]);
+
+
+				}
+			}
+ 
+  		}
+
+  else {
      
-    SDL_FreeSurface(tileset); 
-    SDL_Quit(); 
-    return 0; 
+      // On affiche un message d'erreur si on veut
+      printf("Impossible d'ouvrir le fichier test.txt");
+  	}
+	 SDL_WM_SetCaption("TOWER DEFENSE", NULL);
+	 Afficher(screen,tileset,map);
+
+
+
+
+
+
+
+  do { // attend qu'on appuie sur une touche.
+    
+    SDL_WaitEvent(&event);
+   
+    } while (event.type!=SDL_KEYDOWN);
+
+    SDL_FreeSurface(tileset);
+    SDL_Quit();
+    return 0;
+
 }

c058e3caf52043c5cae650d5dbe70d57ddbde116
lindsayNyr <43404297+lindsayNyr@users.noreply.github.com>
2018-11-28 21:41:31 +0100

    Add files via upload

--- structure.h
+++ structure.h
@@ -1,11 +1,18 @@
 
+#include "define.h"
 
 struct Enemy
 {
   int HP;
   int vitesse;
+  int indiceX; //indice map x
+  int indiceY; //indice map y 
+  int pixel ;
+  int copieMap[WIDTH_MAP][HEIGHT_MAP];
   SDL_Rect Position;
-};
+ 
+} Enemy;
+
 
 
 
